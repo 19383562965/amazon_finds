e12bf0075190ed182582e303458fbbd6b8fb1d60
Timurs <timurs.koleda@gmail.com>
2025-03-15 18:17:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.getElementById('side-menu-button').onclick = function() {
   document.getElementById('side-menu').style = 'transition: 0.5s; right: 0;';
 };
 document.getElementById('side-menu').onmouseleave = function() {
-  document.getElementById('side-menu').style = 'transition: 0.5s; right: -70%;';
+  document.getElementById('side-menu').style = 'transition: 0.5s; right: -100%;';
 };
 document.getElementById('cross-side-menu').onclick = function() {
-  document.getElementById('side-menu').style = 'transition: 0.25s; right: -70%;';
+  document.getElementById('side-menu').style = 'transition: 0.25s; right: -100%;';
 }
 document.getElementById('cart-button').onclick = function() {
   document.getElementById('cart-menu').style = 'transition: 0.5s; right: 0;';
